846f724e67412b856d6fea57decd59fd645f02af
mohanMERNdev <mohanjayabal5@gmail.com>
2024-08-04 02:05:03 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// src/App.js
-
 import React from 'react';
 import WeatherService from './WeatherService';
 import './App.css';
--- src/WeatherService.js
+++ src/WeatherService.js
@@ -8,8 +8,7 @@ class WeatherService extends React.Component {
     error: null,
   };
 
-  apiKey = 'ac7e9a2ca4737d694b2c8cffc21890dd'; // Replace with your OpenWeatherMap API key
-
+  apiKey = 'ac7e9a2ca4737d694b2c8cffc21890dd';
   getWeather = async () => {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${this.apiKey}&units=metric`);
@@ -41,12 +40,30 @@ class WeatherService extends React.Component {
         <button onClick={this.getWeather}>Get Weather</button>
         {this.state.error && <p className="error">{this.state.error}</p>}
         {this.state.weather && (
-          <div className="weather-card">
+          <div className="weather-result">
             <h2>{this.state.weather.name}</h2>
-            <p>Temperature: {this.state.weather.main.temp}°C</p>
-            <p>Weather: {this.state.weather.weather[0].description}</p>
-            <p>Humidity: {this.state.weather.main.humidity}%</p>
-            <p>Wind Speed: {this.state.weather.wind.speed} km/h</p>
+            <div className="weather-details">
+              <div className="weather-card">
+                <img src={`http://openweathermap.org/img/wn/${this.state.weather.weather[0].icon}@2x.png`} alt="Weather icon" />
+                <h3>Temperature</h3>
+                <p>{this.state.weather.main.temp}°C</p>
+              </div>
+              <div className="weather-card">
+                <img src="https://img.icons8.com/ios/50/000000/partly-cloudy-day--v1.png" alt="Weather description icon" />
+                <h3>Weather</h3>
+                <p>{this.state.weather.weather[0].description}</p>
+              </div>
+              <div className="weather-card">
+                <img src="https://img.icons8.com/ios/50/000000/humidity.png" alt="Humidity icon" />
+                <h3>Humidity</h3>
+                <p>{this.state.weather.main.humidity}%</p>
+              </div>
+              <div className="weather-card">
+                <img src="https://img.icons8.com/ios/50/000000/wind.png" alt="Wind speed icon" />
+                <h3>Wind Speed</h3>
+                <p>{this.state.weather.wind.speed} km/h</p>
+              </div>
+            </div>
           </div>
         )}
       </div>
